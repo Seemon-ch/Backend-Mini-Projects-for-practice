cf3e3cdba3228d7fdf3d2bdf4308a22961381a7d
Seemon-ch <seemonchaudhary99@gmail.com>
2026-09-16 14:55:52 +0530

    made update route

--- workWithMongoDB/index.js
+++ workWithMongoDB/index.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const path = require('path');
 const chat = require("./models/chat.js");
 const Chat = require('./models/chat.js');
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname ,"views"));
 app.set("view engine" , "ejs")
 app.use(express.static(path.join(__dirname ,"public")));
 
 app.use(express.urlencoded({extended: true})); //when we submit form to add new data we need to parse the data first.
+app.use(methodOverride("_method"));
 
 
 main().then(()=>{
@@ -23,7 +25,7 @@ async function main(){
 //index route
 app.get("/chats",async (req,res)=>{
     let chats = await Chat.find();
-    console.log(chats);
+    // console.log(chats);
     res.render("index.ejs",{chats});
 });
 
@@ -55,8 +57,16 @@ app.get("/chats/:id/edit" ,async (req,res)=>{
     let chat = await Chat.findById(id);
     res.render("edit.ejs", {chat});
 });
+//Update route
+app.put("/chats/:id",async (req,res)=>{
+    let {id} = req.params;
+    let{msg : newMsg}= req.body;
+    let updatedChat =await Chat.findByIdAndUpdate(id,{msg:newMsg} ,{runValidators:true ,new:true});
+    console.log(updatedChat);
+    res.redirect("/chats");
+});
 
-app.get("/" , (req,res)=>{
+app.get("/" , (req,res)=>{ 
     res.send("WELCOME, go to route '/chats' to see all the chats");
 });
 
